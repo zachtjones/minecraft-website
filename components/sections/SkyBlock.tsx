7581bffcd11b77e73a4e7fd3c5aebe9fbfd53767
Zach Jones <23038709+zachtjones@users.noreply.github.com>
2023-04-02 14:35:29 -0700

    Updated skyblock layout

--- components/sections/SkyBlock.tsx
+++ components/sections/SkyBlock.tsx
@@ -8,7 +8,7 @@ const LATEST_EPISODE = 4
 
 function SkyBlock() {
     const images = [LATEST_EPISODE, LATEST_EPISODE - 1, LATEST_EPISODE - 2].map((number) =>
-        <Grid xs={2} md={1} key={number} style={{ position: 'relative' }}>
+        <Grid item xs={2} md={1} key={number} style={{ position: 'relative' }}>
             <ImageCard
                 src={`/images/Skyblock-${number}.png`}
                 alt={`SkyBlock Episode ${number} Thumbnail`}
@@ -18,7 +18,7 @@ function SkyBlock() {
         </Grid>
     )
     images.push(
-        <Grid xs={2} md={1} key={1} style={{ position: 'relative' }}>
+        <Grid item xs={2} md={1} key={1} style={{ position: 'relative' }}>
             <ImageCard
                 src='/images/Skyblock-more.png'
                 alt="SkyBlock there's more"
@@ -31,22 +31,20 @@ function SkyBlock() {
         </Grid>
     )
     return (
-        <Stack direction='row' sx={{ p: 2 }}>
-            <Grid xs={12} md={4} display='flex'>
-                <Stack direction='column' alignItems='center' sx={{ mt: 4, mr: 4 }} justifyContent='space-around'>
-                    <Image
-                        src='/images/zacari.png'
-                        alt="Zacari's headshot"
-                        width='100'
-                        height='100'
-                    />
-                    <Typography variant="h2">SkyBlock Series</Typography>
-                    <Typography variant="body1" sx={{ mt: 2 }}>
-                        Starting out with a single tree and some grass, let&apos;s see what I can do!
-                    </Typography>
-                    <WatchNowButton url={new URL("https://www.youtube.com/playlist?list=PLNXoRRqPgxuiVrGS_Vw9XbAv9bzQF8lW-")} />
-                </Stack>
-            </Grid>
+        <Stack direction={{ xs: 'column', md: 'row' }} sx={{ p: 2 }}>
+            <Stack direction='column' alignItems='center' sx={{ mt: 4, mr: 4 }} justifyContent='center'>
+                <Image
+                    src='/images/zacari.png'
+                    alt="Zacari's headshot"
+                    width='100'
+                    height='100'
+                />
+                <Typography variant="h2">SkyBlock Series</Typography>
+                <Typography variant="body1" sx={{ mt: 2 }}>
+                    Starting out with a single tree and some grass, let&apos;s see what I can do!
+                </Typography>
+                <WatchNowButton url={new URL("https://www.youtube.com/playlist?list=PLNXoRRqPgxuiVrGS_Vw9XbAv9bzQF8lW-")} />
+            </Stack>
             <Grid xs={12} md={8} container columns={2}>
                 {images}
             </Grid>
