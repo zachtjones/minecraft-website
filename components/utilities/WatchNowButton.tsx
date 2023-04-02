1c6cad7b76d31cd7000c2a8ee6bde6b2b77a2503
Shagun Bose <32908915+shagunBose@users.noreply.github.com>
2023-04-02 13:00:43 -0700

    fix: lint fixed to button

--- components/utilities/WatchNowButton.tsx
+++ components/utilities/WatchNowButton.tsx
@@ -5,10 +5,10 @@ interface WatchNowProps {
     url?: URL;
 }
 
-const WatchNowButton: React.FC<WatchNowProps> = ({url: href = 'https://www.youtube.com/@zacari_mc'}) => {
+const WatchNowButton: React.FC<WatchNowProps> = ({url = 'https://www.youtube.com/@zacari_mc'}) => {
     return (
         <Box sx={{margin: '10px 0px'}}>
-            <Button variant="contained" href={href.toString()} target='_blank'>
+            <Button variant="contained" href={url.toString()} target='_blank'>
             <YouTubeIcon sx={{ marginRight: '5px'}} />
                 Watch Now
             </Button>
